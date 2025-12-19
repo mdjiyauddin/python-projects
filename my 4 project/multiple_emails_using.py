40ad4aab10a79f8032a115f8220a2ec008dd4cf0
mohammed jiyauddin <kbarkat544@gmail.com>
2025-12-19 14:53:02 +0530

    Update multiple_emails_using.py

--- my 4 project/multiple_emails_using.py
+++ my 4 project/multiple_emails_using.py
@@ -1,6 +1,6 @@
 
 
-# Simple Email Sender using Python (smtplib)
+# Simple Email Sender using Python (smtp
 # Author: MD Jiyauddin
 # Description:
 # This script sends an email through Gmail using SMTP protocol.
