46e42297945f59628a7ca227549fe911217d1385
mohammed jiyauddin <kbarkat544@gmail.com>
2025-12-27 08:29:32 +0530

    Update gmail_validation.py

--- second project/gmail_validation.py
+++ second project/gmail_validation.py
@@ -4,7 +4,7 @@ k, j, d = 0, 0, 0
 
 if len(email) >= 6:
     if email[0].isalpha():
-        if ("@" in email) and (email.count("@") == 1): 
+        if ("@" in email) and (email.count("@") == 1):
             if (email[-4] == ".") ^ (email[-3] == "."):
                 for i in email:
                     if i.isspace():
