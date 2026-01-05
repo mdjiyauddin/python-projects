5ccff899d4c83f8ed194d552d33b4b01aa9fb4a7
mohammed jiyauddin <kbarkat544@gmail.com>
2026-01-05 07:43:41 +0530

    Update automessage.py

--- my 7 project/automessage.py
+++ my 7 project/automessage.py
@@ -65,7 +65,7 @@ def wait_for_login(driver):
         logging.error("Timed out waiting for WhatsApp Web login/ready: %s", e)
         raise
 
-# -----------------------
+# ----------------------
 # Send message function
 # -----------------------
 def send_message(driver, phone, message, wait_for_open=10):
