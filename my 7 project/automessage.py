efc6aa14bcaaf059dc0f11df1beb91eefc888045
mohammed jiyauddin <kbarkat544@gmail.com>
2026-01-12 20:23:13 +0530

    Update automessage.py

--- my 7 project/automessage.py
+++ my 7 project/automessage.py
@@ -22,7 +22,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 # -----------------------
 # Configuration / Globals
 # -----------------------
