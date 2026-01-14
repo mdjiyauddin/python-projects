9d41df04443e73ad2479af3c05921814e8054a25
mohammed jiyauddin <kbarkat544@gmail.com>
2026-01-14 20:28:16 +0530

    Update text to handwriting.py

--- my 6 project/text to handwriting.py
+++ my 6 project/text to handwriting.py
@@ -17,6 +17,7 @@ def txt_to_handwriting(text, font_path, output="output.png"):
     print("✅ Saved:", output)
     
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Use: python txt_to_handwriting.py input.txt font.ttf")
