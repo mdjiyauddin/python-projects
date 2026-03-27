1f31e8af39a556aece5b97fa900b3893138078b8
mohammed jiyauddin <kbarkat544@gmail.com>
2026-03-27 21:51:30 +0530

    Update multiple_emails_using.py

--- my 4 project/multiple_emails_using.py
+++ my 4 project/multiple_emails_using.py
@@ -3,7 +3,7 @@
 # Author: MD Jiyauddin
 # Description:
 # This script sends an email through Gmail using SMTP protocol.
-# It demonstrates basic email automation using Pyth
+# It demonstrates basic email automation using Pyt
 
 
 import smtplib as s
