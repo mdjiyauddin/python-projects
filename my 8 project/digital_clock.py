46c2dd3141c1ee55debc441a2171576de13da3fc
mohammad jiyauddin <kbarkat544@gmail.com>
2026-08-22 08:06:36 +0530

    Update digital_clock.py

--- my 8 project/digital_clock.py
+++ my 8 project/digital_clock.py
@@ -17,7 +17,7 @@ time_label = Label(clock, font=("ds-digital", 100, "bold"), bg="yellow", fg="blu
 time_label.pack(pady=50)
 
 
-# Date display label.........
+# Date display label.............
 date_label = Label(clock, font=("times new roman", 30, "bold"), bg="yellow", fg="green")
 date_label.pack()
 
